5d08580665b6a098eff8fb2436486160e3ee4556
cureduck <842483592@qq.com>
2016-11-25 09:52:15 +0800

    爬虫

--- lazy_server.py
+++ lazy_server.py
@@ -1,26 +1,36 @@
 import logging; logging.basicConfig(level=logging.INFO)
-
-import asyncio, os, json, time
-from datetime import datetime
-
+from logger import logger
+import asyncio
 from aiohttp import web
 
-def index(request):
 
-    a=request.headers
-    for k,v in zip(a.keys(),a.values()):
+
+"""
+这是一个只接收request并将request打印出来的模拟器，在127.0.0.1：9000，只接受http协议get方法
+只要点那个运行就可以了
+"""
+async def index(request):
+    headers=dict(**request.headers)                                         #很多类dict而不是dict的子类的类都可以用dict(a)方法或者dict(**a)来使其变成dict类
+    cookie=headers.get('Cookie')
+    if cookie:logger(cookie)
+    for k,v in headers.items():
         print(k,':',v)
+
+    kw = dict(**(await request.post()))
+    for k,v in kw.items():
+        print(k,':',v)
+
     print('--------------------------------------------------------')
 
 
 
     return web.Response(body=b'<h1>Awesome</h1>')
 
-@asyncio.coroutine
-def init(loop):
+
+async def init(loop):
     app = web.Application(loop=loop)
     app.router.add_route('GET', '/', index)
-    srv = yield from loop.create_server(app.make_handler(), '127.0.0.1', 9000)
+    srv = await loop.create_server(app.make_handler(), '127.0.0.1', 9000)
     logging.info('server started at http://127.0.0.1:9000...')
     return srv
 
--- test.py
+++ test.py
@@ -3,9 +3,17 @@ from urllib import request
 import gzip
 
 
+"""
+这个cookies真是操蛋，它并不能读取本地cookies文件，这tm有卵用啊？
+cookie=cookiejar.CookieJar()
+handler=request.HTTPCookieProcessor(cookie)
+opener=request.build_opener(handler)
+opener.open(req)
+姑且先把方法记下来吧
+不知道python有没有读取本地cookie的lib有的话可能还能用到
+"""
 
-
-def get_content(url,headers):
+def get_content(url,headers=None):
     req = request.Request(url)
     req.headers = headers
     cookie = cookiejar.CookieJar()
@@ -33,9 +41,8 @@ headers={
     'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
     #'Referer':'http://www.pixiv.net/member_illust.php?mode=medium&illust_id=60052998&uarea=daily',
     'Accept-Encoding':'gzip, deflate, sdch',
-    'Accept-Language':'zh-CN,zh;q=0.8'
-    #'Cookie':'p_ab_id=0; login_ever=yes; module_orders_mypage=%5B%7B%22name%22%3A%22spotlight%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22everyone_new_illusts%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22sensei_courses%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22hot_entries%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22featured_tags%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22contests%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22following_new_illusts%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22mypixiv_new_illusts%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22booth_follow_items%22%2C%22visible%22%3Atrue%7D%5D; PHPSESSID=20981040_e5430321213a59e745c91c0efd73a56c; __utma=235335808.383889308.1478570004.1479887981.1479894803.10; __utmb=235335808.4.10.1479894803; __utmz=235335808.1479790613.5.3.utmcsr=baidu|utmccn=(organic)|utmcmd=organic; __utmv=235335808.|2=login%20ever=yes=1^3=plan=normal=1^5=gender=male=1^6=user_id=20981040=1; _ga=GA1.2.383889308.1478570004'
+    'Accept-Language':'zh-CN,zh;q=0.8',
+    'Cookie':'p_ab_id=0; login_ever=yes; module_orders_mypage=%5B%7B%22name%22%3A%22spotlight%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22everyone_new_illusts%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22sensei_courses%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22hot_entries%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22featured_tags%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22contests%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22following_new_illusts%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22mypixiv_new_illusts%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22booth_follow_items%22%2C%22visible%22%3Atrue%7D%5D; PHPSESSID=20981040_e5430321213a59e745c91c0efd73a56c; __utma=235335808.383889308.1478570004.1479887981.1479894803.10; __utmb=235335808.4.10.1479894803; __utmz=235335808.1479790613.5.3.utmcsr=baidu|utmccn=(organic)|utmcmd=organic; __utmv=235335808.|2=login%20ever=yes=1^3=plan=normal=1^5=gender=male=1^6=user_id=20981040=1; _ga=GA1.2.383889308.1478570004'
 }
 
-data=get_content('http://www.liaoxuefeng.com/wiki/0014316089557264a6b348958f449949df42a6d3a2e542c000/001432688314740a0aed473a39f47b09c8c7274c9ab6aee000',headers)
-data=get_content('http://baidu.com',headers)
+data=get_content('http://127.0.0.1:9000', headers)
